1b5371bd07779b23e99e0523d4f4d770ec256a4c
devkavin <kavindra.senanayake@gmail.com>
2024-08-12 17:48:15 +0530

    course user pivot table created

---
+++ database/migrations/2024_08_12_084158_create_course_user_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('course_user', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('course_id')->unsigned();
+            $table->bigInteger('student_id')->unsigned();
+            $table->foreign('course_id')->references('id')->on('courses');
+            $table->foreign('student_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('course_user');
+    }
+};
